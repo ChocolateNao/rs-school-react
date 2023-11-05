3d6fbae9604983de73e2610aabe8b7b1080f03f3
Andrey Loginov <andrey12q112@gmail.com>
2023-11-06 00:23:56 +0300

    fix: edit list's appearance

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -17,12 +17,10 @@ function AnimeCard({ anime }: AnimeCardProps) {
       <div className="card">
         <img src={images.webp.image_url} alt={title} className="card__image" />
         <div className="card__description">
-          <p>Name: {title}</p>
-          <p className="card__status">
-            Status: {airing ? 'Airing' : 'Completed'}
-          </p>
-          <p>Score: {score}</p>
-          <p>Type: {type}</p>
+          <b>Name:</b> <p>{title}</p>
+          <b>Status:</b> <p>{airing ? 'Airing' : 'Completed'}</p>
+          <b>Score:</b> <p>{score}</p>
+          <b>Type:</b> <p>{type}</p>
         </div>
       </div>
     </Link>
