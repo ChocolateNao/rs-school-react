79bf418ea2c18c14640437750840c606524e2d46
Andrey Loginov <andrey12q112@gmail.com>
2023-11-05 16:18:48 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
+import ErrorFallback from './components/ErrorFallback';
 import App from './App';
 
 import './index.css';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorFallback />} />
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
