1f03d57df7a512ade0a0b5bfa677e5afc17801e1
Andrey Loginov <andrey12q112@gmail.com>
2023-11-05 18:43:41 +0300

    refactor: wrap logic into Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,14 @@
-import './App.css';
-import React, { ChangeEvent, Component } from 'react';
-import { Character } from './resources/Character.interface';
-import CharacterCard from './components/CharacterCard';
-import Search from './components/Search';
-import Loading from './components/Loading';
 import ErrorBoundary from './components/ErrorBoundary';
-import Button from './components/Button';
-import Error from './components/Error';
+import Main from './components/Main';
 
-interface AppState {
-  userInput: string;
-  data: Character[];
-  loading: boolean;
-  error: string | null;
-  dummyError: boolean;
-}
+import './App.css';
 
-class App extends Component<object, AppState> {
-  constructor(props: AppState) {
-    super(props);
-    this.state = {
-      userInput: localStorage.getItem('userInput') ?? '',
-      data: [],
-      loading: false,
-      error: null,
-      dummyError: false,
-    };
-  }
-
-  componentDidMount() {
-    const savedInput = localStorage.getItem('userInput');
-    if (savedInput) {
-      this.setState({ userInput: savedInput });
-    }
-    this.fetchData();
-  }
-
-  handleButtonClick = () => {
-    const { userInput } = this.state;
-    this.setState({ error: null, data: [] });
-    localStorage.setItem('userInput', userInput);
-    this.fetchData();
-  };
-
-  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ userInput: event.target.value });
-  };
-
-  handleErrorThrow = () => {
-    this.setState({ dummyError: true });
-  };
-
-  fetchData = () => {
-    const { userInput } = this.state;
-    this.setState({ loading: true });
-    fetch(
-      `https://rickandmortyapi.com/api/character/?page=1${
-        userInput ? `&name=${userInput}` : ''
-      }`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          this.setState({ loading: false, error: data.error });
-          return;
-        }
-        this.setState({ data: data.results, loading: false });
-      })
-      .catch((error) =>
-        this.setState({ loading: false, error: error.message })
-      );
-  };
-
-  render() {
-    const { userInput, data, loading, error, dummyError } = this.state;
-    return (
-      <ErrorBoundary>
-        <header className="header">
-          <Search
-            userInput={userInput}
-            onButtonClick={this.handleButtonClick}
-            onInputChange={this.handleInputChange}
-          />
-          <Button className="header__throw" onClick={this.handleErrorThrow}>
-            Throw error
-          </Button>
-        </header>
-
-        {loading && <Loading />}
-        {error && <div>{error}</div>}
-        {dummyError && <Error />}
-        {data && (
-          <section className="cards">
-            {data.map((item) => (
-              <CharacterCard key={item.id} character={item} />
-            ))}
-          </section>
-        )}
-      </ErrorBoundary>
-    );
-  }
+function App() {
+  return (
+    <ErrorBoundary>
+      <Main />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
