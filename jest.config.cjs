66483d9c706e01f94bbcc112b41c6b36ecd536fe
Andrey Loginov <andrey12q112@gmail.com>
2023-11-18 01:05:14 +0300

    test: make jest understand aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^ui/(.*)$': '<rootDir>/src/shared/ui/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^api/(.*)$': '<rootDir>/src/api/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^resources/(.*)$': '<rootDir>/src/resources/$1',
+    '^shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^context/(.*)$': '<rootDir>/src/shared/context/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   coverageThreshold: {
