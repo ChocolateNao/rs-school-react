ba683d1c924267a52d0a941b71d54c43b52bc99e
Andrey Loginov <andrey12q112@gmail.com>
2023-11-19 23:22:41 +0300

    feat: set up redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import SearchProvider from 'context/SearchContext';
+import { Provider } from 'react-redux';
+
 import Main from 'pages/Main';
 import ErrorBoundary from 'shared/ErrorBoundary';
+import store from 'shared/store';
 
 import './App.css';
 
 function App() {
   return (
     <ErrorBoundary>
-      <SearchProvider>
+      <Provider store={store}>
         <Main />
-      </SearchProvider>
+      </Provider>
     </ErrorBoundary>
   );
 }
