f420ca4f3a45ce7ae12a492e6d32be8e26e0ca07
ChocolateNao <andrey12q112@gmail.com>
2023-11-26 05:04:10 +0300

    feat: add image source host

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: './dist',
+  images: {
+    remotePatterns: [{
+      hostname: 'cdn.myanimelist.net'
+    }],
+  },
 };
 
 module.exports = nextConfig;
