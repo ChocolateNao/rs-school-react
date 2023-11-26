281cd48e0e3d03d4cd11fd0e54fd936c4b8a1158
ChocolateNao <andrey12q112@gmail.com>
2023-11-26 05:11:20 +0300

    feat: add hook to handle query params

---
+++ src/hooks/useUpdateQueryParams.ts
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
+
+function useUpdateQueryParams() {
+  const router = useRouter();
+  const prevParamsRef = useRef<ParsedUrlQuery | null>(null);
+
+  const updateQueryParams = (params: ParsedUrlQuery) => {
+    const updatedParams = { ...router.query, ...params };
+    Object.keys(updatedParams).forEach((key) => {
+      if (updatedParams[key] === '') {
+        delete updatedParams[key];
+      }
+    });
+    router.push(
+      {
+        pathname: router.pathname,
+        query: updatedParams,
+      },
+      undefined
+    );
+  };
+
+  useEffect(() => {
+    prevParamsRef.current = router.query;
+  }, [router.query]);
+
+  return { updateQueryParams, prevParams: prevParamsRef.current };
+}
+
+export default useUpdateQueryParams;
