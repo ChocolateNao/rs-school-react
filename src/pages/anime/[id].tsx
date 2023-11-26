0ea5670a86ac6e66a00749f0ead07be6b6c2c582
ChocolateNao <andrey12q112@gmail.com>
2023-11-26 17:51:46 +0300

    feat: update pages, add layout

---
+++ src/pages/anime/[id].tsx
@@ -0,0 +1,41 @@
+import { wrapper } from 'store/index';
+import { getAnimeById, getAnimeList, jikanApi } from 'store/jikanApi';
+
+import AnimeDetails from 'components/AnimeDetails';
+import { IAnime } from 'models/Anime.interface';
+import { IAnimeDetails } from 'models/AnimeDetails.interface';
+import { IPaginationData } from 'models/Pagination.interface';
+import Layout from 'pages/layout';
+
+interface DetailsProps {
+  animeList: { data: IAnime[]; pagination: IPaginationData };
+  animeDetails: { data: IAnimeDetails };
+}
+
+export default function Details({ animeList, animeDetails }: DetailsProps) {
+  return (
+    <Layout animeList={animeList}>
+      <AnimeDetails animeDetails={animeDetails?.data} />
+    </Layout>
+  );
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { page, per, search, id } = context.query;
+
+    const querystring = `${search ? `?q=${String(search).trim()}` : '?q='}${
+      page ? `&page=${page}` : ''
+    }${per ? `&limit=${per}` : ''}`;
+    const details = await store.dispatch(getAnimeById.initiate(String(id)));
+    const list = await store.dispatch(getAnimeList.initiate(querystring));
+
+    await Promise.all(store.dispatch(jikanApi.util.getRunningQueriesThunk()));
+    return {
+      props: {
+        animeList: list?.data || undefined,
+        animeDetails: details?.data || undefined,
+      },
+    };
+  }
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { wrapper } from 'store/index';
+import { getAnimeList, jikanApi } from 'store/jikanApi';
+
+import { IAnime } from 'models/Anime.interface';
+import { IPaginationData } from 'models/Pagination.interface';
+
+import Layout from './layout';
+
+interface HomeProps {
+  animeList: { data: IAnime[]; pagination: IPaginationData };
+}
+
+export default function Home({ animeList }: HomeProps) {
+  return <Layout animeList={animeList} />;
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { page, per, search } = context.query;
+    const querystring = `${search ? `?q=${String(search).trim()}` : '?q='}${
+      page ? `&page=${page}` : ''
+    }${per ? `&limit=${per}` : ''}`;
+    const list = await store.dispatch(getAnimeList.initiate(querystring));
+
+    await Promise.all(store.dispatch(jikanApi.util.getRunningQueriesThunk()));
+    return {
+      props: {
+        animeList: list?.data,
+      },
+    };
+  }
+);
