e67b9d4d7de7ff2cf89b40ca23a70b7b7034278e
ChocolateNao <andrey12q112@gmail.com>
2023-12-04 01:01:18 +0300

    feat: add header styles and component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
 
+import styles from './Header.module.scss';
+
 function Header() {
   return (
-    <header className="header">
-      <h1 className="header__title">React Forms Assignment</h1>
-      <Link to="/">Home</Link>
-      <Link to="/controlled">Controlled form</Link>
-      <Link to="/uncontrolled">Uncontrolled form</Link>
+    <header className={styles.header}>
+      <h1 className={styles.header__title}>React Forms Assignment</h1>
+      <nav className={styles.header__nav}>
+        <Link to="/" className={styles.header__link}>
+          Home
+        </Link>
+        <Link to="/controlled" className={styles.header__link}>
+          Controlled form
+        </Link>
+        <Link to="/uncontrolled" className={styles.header__link}>
+          Uncontrolled form
+        </Link>
+      </nav>
     </header>
   );
 }
